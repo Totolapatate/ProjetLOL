54d315307c49f89b373d5c2d3c80f25eef013bf9
Totolapatate <thomasclaire3192@gmail.com>
2024-02-09 17:19:33 +0100

    Premier commit avec le debut du projet (main)

---
+++ main.py
@@ -0,0 +1,14 @@
+Champion1 = {"Nom": "Jax", "Niveau": 1, "Pièce d'or": 500, "Points de vie maximum": 665, "Points de vie courants": 665, "Attaque": 68, "Armure": 36, "Points de mana maximum": 339, "Points de mana courants": 339, "Vitesse de déplacement": 350}
+Champion2 = {"Nom": "Olaf", "Niveau": 1, "Pièce d'or": 500, "Points de vie maximum": 645, "Points de vie courants": 645, "Attaque": 68, "Armure": 35, "Points de mana maximum": 316, "Points de mana courants": 316, "Vitesse de déplacement": 350}
+
+def Afficher_Statistiques(Champion1, Champion2):
+  Question = input("De quel champion voulez-vous voir les statistiques ?")
+  print(Question)
+  if  Champion1 == input:
+    print(Champion1)
+  else :
+    print(Champion2)
+
+#def Utiliser_Potion(PV, Mana):
+  #if Points de vie courant + 125 > Point de vie max:
+    #Point de vie max - Point de vie courant = PV
